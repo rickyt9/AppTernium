4d42d3b085c5d1fc0408887c922f813ec3d613eb
Ricardo Rodriguez <ricardo.rodriguez.rdz9@gmail.com>
2021-05-14 15:27:31 -0500

    Agregado funcion de post para el login

--- AppTernium/Pages/Index.cshtml.cs
+++ AppTernium/Pages/Index.cshtml.cs
@@ -17,5 +17,34 @@ namespace AppTernium.Pages {
         public void OnGet() {
 
         }
+
+        [BindProperty]
+        public Login Usuario { get; set; }
+        public string Mensaje { get; set; }
+
+        public async Task<IActionResult> OnPost() {
+            string responseContent = "[]";
+
+            // Buscamos el recurso
+            Uri baseURL = new Uri("https://chatarrap-api.herokuapp.com/users/login");
+
+            // Creamos el cliente para que haga nuestra peticion
+            HttpClient client = new HttpClient();
+
+            // Armamos nuestra peticion
+            JObject joPeticion = new JObject();
+            joPeticion.Add("username", Usuario.username);
+            joPeticion.Add("password", Usuario.password);
+
+            var stringContent = new StringContent(joPeticion.ToString(), Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await client.PostAsync(baseURL.ToString(), stringContent);
+
+            if (response.IsSuccessStatusCode) {
+                responseContent = await response.Content.ReadAsStringAsync();
+            }
+
+            return RedirectToPage("Response", new { result = responseContent });
+        }
     }
 }
