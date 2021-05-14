97136edd000de93a065abd2abbe7b616bdc27d4d
Ricardo Rodriguez <ricardo.rodriguez.rdz9@gmail.com>
2021-05-14 15:34:22 -0500

    Agregado dos atributos en Login

--- AppTernium/Models/Login.cs
+++ AppTernium/Models/Login.cs
@@ -7,5 +7,7 @@ namespace AppTernium.Models {
     public class Login {
         public string username { get; set; }
         public string password { get; set; }
+        public string key { get; set; }
+        public string id { get; set; }
     }
 }
