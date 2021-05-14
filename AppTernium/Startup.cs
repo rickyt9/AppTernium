356bb12e013f2cfd8a0ad7f52abc6ac38c30bf76
Ricardo Rodriguez <ricardo.rodriguez.rdz9@gmail.com>
2021-05-14 15:13:08 -0500

    Agregada la configuración para la sesión

--- AppTernium/Startup.cs
+++ AppTernium/Startup.cs
@@ -20,6 +20,12 @@ namespace AppTernium {
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
             services.AddRazorPages();
+            services.AddDistributedMemoryCache();
+            services.AddSession(options => {
+                options.IdleTimeout = TimeSpan.FromSeconds(10);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +44,8 @@ namespace AppTernium {
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSession();
 
             app.UseEndpoints(endpoints => {
                 endpoints.MapRazorPages();
