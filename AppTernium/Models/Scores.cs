990926cffb2d5bf0e2bee2fee1388c7a73c404a1
Ricardo Rodriguez <ricardo.rodriguez.rdz9@gmail.com>
2021-05-15 02:04:12 -0500

    Creada la clase de scores

---
+++ AppTernium/Models/Scores.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppTernium.Models {
+    public class Scores {
+        public string username { get; set; }
+        public int score { get; set; }
+    }
+}
