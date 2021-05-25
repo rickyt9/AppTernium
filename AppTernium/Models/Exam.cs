368856c99052ce20a7bf80108e0c9fb3bf541c55
davidsanmag <A01720476@itesm.mx>
2021-05-25 16:21:37 -0500

    backend examenes

---
+++ AppTernium/Models/Exam.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppTernium.Models {
+    public class Exam {
+        public IList<string> images { get; set; }
+        public IList<string> access { get; set; }
+        public IList<string> presented { get; set; }
+        public string _id { get; set; }
+        public string examName { get; set; }
+        public string description { get; set; }
+        public string size { get; set; }
+        public string attempts { get; set; }
+        public DateTime date { get; set; }
+        public DateTime createdAt { get; set; }
+        public DateTime updatedAt { get; set; }
+        public int __v { get; set; }
+        public DateTime dueDate { get; set; }
+        public string user { get; set; }
+    }
+}
