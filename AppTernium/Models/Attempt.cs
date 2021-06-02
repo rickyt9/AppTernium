bb968f7f1b7b7bf4d7c158c08954a6fa383de4ec
davidsanmag <A01720476@itesm.mx>
2021-06-02 15:33:16 -0500

    backend profile

---
+++ AppTernium/Models/Attempt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppTernium.Models
+{
+    public class Attempt
+    {
+        public string _id { get; set; }
+        public string username { get; set; }
+        public string examName { get; set; }
+        public string examID { get; set; }
+        public int correct { get; set; }
+        public int questions { get; set; }
+        public int score { get; set; }
+        public int attempt { get; set; }
+        public DateTime date { get; set; }
+        public DateTime createdAt { get; set; }
+        public DateTime updatedAt { get; set; }
+        public int __v { get; set; }
+    }
+}
